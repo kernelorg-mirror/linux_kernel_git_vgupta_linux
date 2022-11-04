d9237e5b0715332fec4930b09e23c3ea204cea13
Greentime Hu <greentime.hu@sifive.com>
2022-11-04 12:59:24 -0700

    riscv: introduce Vector enable/disable helpers
    These are small and likely to be frequently called so implement as
    inline routines (vs. function call).
    
    Also for consistency, create both "C" and asm variants.
    
    Co-developed-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Co-developed-by: Vincent Chen <vincent.chen@sifive.com>
    Signed-off-by: Vincent Chen <vincent.chen@sifive.com>
    Signed-off-by: Greentime Hu <greentime.hu@sifive.com>
    Signed-off-by: Vineet Gupta <vineetg@rivosinc.com>
    [vineetg: create new patch from meshup, introduced asm variant]

--- arch/riscv/include/asm/vector.h
+++ arch/riscv/include/asm/vector.h
@@ -10,6 +10,20 @@
 #include <linux/types.h>
 #include <asm/csr.h>
 
+#ifdef __ASSEMBLY__
+
+.macro RVV_ENABLE reg
+	li      \reg, SR_VS
+	csrs    CSR_STATUS, \reg
+.endm
+
+.macro RVV_DISABLE reg
+	li      \reg, SR_VS
+	csrc    CSR_STATUS, \reg
+.endm
+
+#else /* ! __ASSEMBLY__ */
+
 #ifdef CONFIG_RISCV_ISA_V
 
 extern struct static_key_false riscv_isa_ext_key_vector;
@@ -19,10 +33,22 @@ static __always_inline bool has_vector(void)
        return static_branch_likely(&riscv_isa_ext_key_vector);
 }
 
-#else /* ! CONFIG_RISV_ISA_V */
+static inline void rvv_enable(void) {
+	unsigned int vs = SR_VS;
+	csr_set(CSR_STATUS, vs);
+}
+
+static inline void rvv_disable(void) {
+	unsigned int vs = SR_VS;
+	csr_clear(CSR_STATUS, vs);
+}
+
+#else /* ! CONFIG_RISCV_ISA_V */
 
 static __always_inline bool has_vector(void) { return false; }
 
 #endif /* ! CONFIG_RISCV_ISA_V */
+
+#endif /* ! __ASSEMBLY__ */
 
 #endif /* ! __ASM_RISCV_VECTOR_H */
